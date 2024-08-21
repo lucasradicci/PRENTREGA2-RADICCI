f55fc63b3917244b600c2695b73b88721224d04b
lucas radicci <radicci.lukas@gmail.com>
2024-08-21 12:41:23 -0300

    agregue un array de prductos, con una variable y sus id

---
+++ main.js
@@ -0,0 +1,12 @@
+//Array de productos 
+const productos=[
+    {id:1, nombre: "LAVARROPAS", precio:500, stock:15},
+    {id:2, nombre: "HELADERA", precio:700, stock:10},
+    {id:3, nombre: "TELEVISOR", precio:400, stock:12},
+    {id:4, nombre: "MICROONDAS", precio:500, stock:8},
+    {id:5, nombre: "HORNO ELECTRICO", precio:200, stock:17},
+];
+console.log(productos);
+
+
+
